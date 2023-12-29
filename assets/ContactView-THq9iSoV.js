3bb3c55438a5b60595b8ee02f8ea2daef28433f9
dandan <itsdansantos.qpax@gmail.com>
2023-12-29 12:56:10 +0800

    Portfolio-projectpic

---
+++ assets/ContactView-THq9iSoV.js
@@ -0,0 +1,1 @@
+import{_ as c,o as t,c as e}from"./index-3GajW8Cz.js";const o={},s={class:"contact"};function n(a,r){return t(),e("div",s," This is Contact ")}const i=c(o,[["render",n]]);export{i as default};
